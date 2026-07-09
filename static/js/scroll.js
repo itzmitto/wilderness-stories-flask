d4ba02d11c86e3ba9ad769349cc7ac4d3980f529
itzmitto <andre.babirian2004@gmail.com>
2026-07-09 16:14:42 +0200

    navbar styling aangepast

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -1,17 +1,27 @@
 const observer = new IntersectionObserver((entries) => {
-
     entries.forEach((entry) => {
-
         if (entry.isIntersecting) {
             entry.target.classList.add("show");
         }
-
     });
-
 }, {
     threshold: 0.2
 });
-
 document.querySelectorAll(".reveal").forEach((element) => {
     observer.observe(element);
+});
+
+const hero = document.querySelector(".hero");
+const heroContent = document.querySelector(".parallax-content");
+window.addEventListener("scroll", () => {
+    const scrollY = window.scrollY;
+    if (hero) {
+        hero.style.setProperty("--scroll", scrollY);
+        heroContent.style.transform =
+            `translateY(${scrollY * 0.18 - 40}px)`;
+        hero.style.setProperty(
+            "--hero-scale",
+            1.08 - scrollY * 0.00008
+        );
+    }
 });
