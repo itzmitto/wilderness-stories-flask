519b7351bc885cabc9d9fc8235bbc70e570c40f6
itzmitto <andre.babirian2004@gmail.com>
2026-07-09 16:11:23 +0200

    styling van de 2de pagina aangepast

---
+++ static/js/scroll.js
@@ -0,0 +1,17 @@
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+
+    });
+
+}, {
+    threshold: 0.2
+});
+
+document.querySelectorAll(".reveal").forEach((element) => {
+    observer.observe(element);
+});
