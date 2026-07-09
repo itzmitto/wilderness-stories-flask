cf6e28abbddd5911ed452299b3255e92b49d05d4
itzmitto <andre.babirian2004@gmail.com>
2026-07-09 16:18:47 +0200

    desert en ocean pagina toegevoegd plus styling toegevoegd

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,15 @@
-console.log("Flask werkt!");
+const menuButton = document.getElementById("menuButton");
+const closeButton = document.getElementById("closeButton");
+const fullscreenMenu = document.getElementById("fullscreenMenu");
+
+menuButton.addEventListener("click", () => {
+
+    fullscreenMenu.classList.add("active");
+
+});
+
+closeButton.addEventListener("click", () => {
+
+    fullscreenMenu.classList.remove("active");
+
+});
